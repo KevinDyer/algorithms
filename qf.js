0e429d4e273482fabef3c439b52e9c359f93743f
SKiddem <kevin.m.dyer@gmail.com>
2015-01-03 13:08:03 -0700

    Added weighted quick find with path compression.

---
+++ qf.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+  var QuickUnion = function (N) {
+    this.id = [];
+    this.sz = [];
+    for (var i = 0; i < N; i++) {
+      this.id.push(i);
+    }
+  };
+
+  QuickUnion.prototype.root = function (i) {
+    while (i != this.id[i]) {
+      this.id[i] = this.id[this.id[i]];
+      i = this.id[i];
+    }
+    return i;
+  };
+
+  QuickUnion.prototype.find = function (p ,q) {
+    return this.root(p) === this.root(q);
+  };
+
+  QuickUnion.prototype.union = function (p, q) {
+    var i = this.root(p);
+    var j = this.root(q);
+    if (this.sz[i] < this.sz[j]) {
+      this.id[i] = j;
+      this.sz[j] += this.sz[i];
+    } else {
+      this.id[j] = i;
+      this.sz[i] += this.sz[j];
+    }
+  };
+})();
--- uf.js
+++ uf.js
@@ -4,15 +4,18 @@
   var UnionFind = function (N) {
     this.id = [];
     for (var i = 0; i < N; i++) {
-      this.id[i] = i;
+      this.id.push(i);
     }
   };
-  
-  UnionFind.prototype.union = function(n, m) {
 
+  UnionFind.prototype.find = function (p, q) {
+    return this.id[p] === this.id[q];
   };
-
-  UnionFind.prototype.find = function(n, m) {
-
+  
+  UnionFind.prototype.union = function (p, q) {
+    var pid = this.id[p];
+    for (var i = 0; i < this.id.length; i++) {
+      if (this.id[i] === pid) this.id[i] = this.id[q];
+    }
   };
 })();
