0beb4f29e0e4ee3bfcf9c4162b91a4efe7588568
kevin <kevin.m.dyer@gmail.com>
2015-02-22 12:01:05 -0700

    Starting to work on BinaryTree

--- bt.js
+++ bt.js
@@ -1,10 +1,10 @@
 'use strict';
 
 (function() {
-  var Node = function(root, data) {
-    this.data = data;
-
-    this.root = root;
+  var Node = function(key, value) {
+    this.key = key;
+    this.value = value;
+    this.count = 0;
     this.left = null;
     this.right = null;
   };
@@ -13,27 +13,51 @@
     this.root = null;
   };
 
-  BinaryTree.prototype.insert = function(data) {
-    if (null === this.root) {
-      this.root = new Node(null, data);
-    } else {
-      this.insert(this.root, data);
-    }
+  BinaryTree.prototype.put = function(key, value) {
+    this.root = this._insert(this.root, key, value);
   };
 
-  BinaryTree.prototype.insert = function(root, data) {
-    if (data < root.data) {
-      if (null === root.left) {
-        root.left = new Node(root, data);
-      } else {
-        this.insert(root.left, data);
-      }
+  BinaryTree.prototype._put = function(x, key, value) {
+    if (null === x) {
+      return new Node(key, value);
+    }
+    var cmp = key.compareTo(x.key);
+    if (0 > cmp) {
+      x.left = this._put(x.left, key, value);
+    } else if (0 < cmp) {
+      x.right = this._put(x.right, key, value);
     } else {
-      if (null === root.right) {
-        root.right = new Node(root, data);
+      x.value = value;
+    }
+    return x;
+  }
+
+  BinaryTree.prototype.get = function(key) {
+    var x = this.root;
+    while (null === x) {
+      var cmp = key.compareTo(x.key);
+      if (0 > cmp) {
+        x = x.left;
+      } else if (0 < cmp) {
+        x = x.right;
       } else {
-        this.insert(root.right, data);
+        return x.value;
       }
     }
+    return null;
+  };
+
+  BinaryTree.prototype.size = function() {
+    return this._size(this.root);
+  }
+
+  BinaryTree.prototype._size = function(x) {
+    if (null === x) {
+      return 0;
+    }
+    return x.count;
+  }
+
+  BinaryTree.prototype.delete = function(key) {
   };
 })();
